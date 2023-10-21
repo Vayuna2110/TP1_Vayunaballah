0bda07e75ad0878533644f409a45189ff713a3af
Vayuna2110 <vayuna.fm@gmail.com>
2023-10-21 22:33:28 +0400

    editing file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'dart:io' if (dart.library.io) 'dart:ui';
+
 
 
 void main() {
