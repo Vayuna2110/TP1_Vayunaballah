b5548f1cc9bae2df4ebd03dfdcf7e112e3729193
Vayuna2110 <vayuna.fm@gmail.com>
2023-10-21 22:03:38 +0400

    Fixing errors on file  main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:ui';
 
 void main() {
   runApp(MyApp());
